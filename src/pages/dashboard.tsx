a629753f44650d724c591548fd06ab31b758869c
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2023-03-31 11:46:23 -0300

    Relizando refresh do token

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,8 +1,13 @@
 import { AuthContext } from "@/contexts/AuthContext";
-import { useContext } from "react";
+import { api } from "@/services/api";
+import { useContext, useEffect } from "react";
 
 export default function Dashboard() {
   const { user } = useContext(AuthContext);
+
+  useEffect(() => {
+    api.get('/me').then(response => console.log(response));
+  }, []);
 
   return (
     <h1>Dashboard: {user?.email}</h1>
